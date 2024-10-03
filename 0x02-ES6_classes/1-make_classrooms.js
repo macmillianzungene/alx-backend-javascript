7c42c11bf87f02e7ae2199bbcbf8c40516af9ac6
macmillianzungene <macmillianzungene@gmail.com>
2024-10-03 21:13:30 +0000

    updated 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,11 @@
 // Import the ClassRoom class from 0-classroom.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 // Implement the initializeRooms function
-export function initializeRooms() {
+export default function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
 }
-
