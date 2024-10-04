5e38297982adc691cbb9c89946e4c510f43c91c5
macmillianzungene <macmillianzungene@gmail.com>
2024-10-04 00:26:59 +0000

    updated

---
+++ 0x02-ES6_classes/6-main.js
@@ -0,0 +1,19 @@
+import Building from './5-building';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate the building immediately! This building has ${this._floors} floors.`;
+  }
+}
+
+export default SkyHighBuilding;
+
--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,18 +2,18 @@
 import Building from './5-building.js';
 
 class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get floors() {
-        return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors.`;
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
 }
 
 // Example usage
@@ -21,4 +21,3 @@ const skyHigh = new SkyHighBuilding(10000, 50);
 console.log(skyHigh.sqft); // 10000
 console.log(skyHigh.floors); // 50
 console.log(skyHigh.evacuationWarningMessage()); // "Evacuate slowly the 50 floors."
-
