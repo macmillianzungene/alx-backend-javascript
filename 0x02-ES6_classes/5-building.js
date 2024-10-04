567ecf11991b3f057f6a8dcd239666709514a066
macmillianzungene <macmillianzungene@gmail.com>
2024-10-04 00:00:26 +0000

    updated

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,44 +1,31 @@
 class Building {
-    constructor(sqft) {
-        if (new.target === Building) {
-            throw new Error("Cannot instantiate abstract class Building directly");
-        }
-        this._sqft = sqft;
-    }
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    // Abstract method
-    evacuationWarningMessage() {
-        throw new Error("Class extending Building must override evacuationWarningMessage");
-    }
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    return 'Evacuate the building immediately!';
+  }
 }
 
-// Example of a subclass
 class OfficeBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get floors() {
-        return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    evacuationWarningMessage() {
-        return "Please evacuate the office building immediately!";
-    }
+  evacuationWarningMessage() {
+    return `Evacuate the building immediately! This building has ${this._floors} floors.`;
+  }
 }
 
-// Example usage
-try {
-    const office = new OfficeBuilding(5000, 10);
-    console.log(office.sqft); // 5000
-    console.log(office.floors); // 10
-    console.log(office.evacuationWarningMessage()); // "Please evacuate the office building immediately!"
-} catch (error) {
-    console.error(error.message);
-}
-
+export default OfficeBuilding;
