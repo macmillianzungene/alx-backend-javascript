ad40e2c5ab420bebf2982f347987c95212df0549
macmillianzungene <macmillianzungene@gmail.com>
2024-10-06 17:19:15 +0000

    updated

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
-// eslint-disable-next-line no-unused-vars
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user]).then((value) => { console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`); }).catch(() => { console.log('Signup system offline'); });
 }
