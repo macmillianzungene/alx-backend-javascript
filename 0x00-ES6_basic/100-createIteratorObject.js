9287aeb66494fc8a4112cc3df57025ae80b4c749
macmillianzungene <macmillianzungene@gmail.com>
2024-10-06 19:35:07 +0000

    updated

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,20 +1,9 @@
 export default function createIteratorObject(report) {
-  let departmentIndex = 0;
-  let employeeIndex = 0;
-  return {
-    next() {
-      while (departmentIndex < report.departments.length) {
-        const department = report.departments[departmentIndex];
-
-        if (employeeIndex < department.employees.length) {
-          const employee = department.employees[employeeIndex];
-          employeeIndex += 1;
-          return { value: employee, done: false };
-        }
-        departmentIndex += 1;
-        employeeIndex = 0;
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
-      return { done: true };
-    },
-  };
+    }
+  }());
 }
